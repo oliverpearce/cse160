cf3b0ab4fd3fb59e15b123b105ab1a846e6149e9
Oliver Pearce <OliverPearce13@gmail.com>
2025-03-11 02:32:37 -0700

    pls work mane

--- asgn5/asgn5.js
+++ asgn5/asgn5.js
@@ -1,4 +1,4 @@
-import './style.css'
+import './asgn5.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
